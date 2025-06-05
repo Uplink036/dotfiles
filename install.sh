105049365c2c47861e31e30f39e2173fcd9d4ade
Uplink036 <olsj21@student.bth.se>
2025-06-05 09:57:34 +0200

    Add new install files

--- install.sh
+++
@@ -1,4 +0,0 @@
-sudo apt install curl make
-./install_nvim.sh 
-./install_tmux.sh
-./install_zoxide.sh
---
+++ install_gnome_extensions.sh
@@ -0,0 +1,2 @@
+sudo apt install chrome-gnome-shell
+# After which you can install useful stuff such as caffeine.
